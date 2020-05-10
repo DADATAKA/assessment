743107e6e43c0d4eecca5f7aabc248856b77f978
DADATAKA <fukuda.04.taka@gmail.com>
2020-05-10 11:25:50 +0900

    Create assessment.js

---
+++ assessment.js
@@ -0,0 +1,56 @@
+'use strict';
+const userNameInput = document.getElementById('user-name');
+const assessmentButton = document.getElementById('assessment');
+const resultDivided = document.getElementById('result-area');
+const tweetDivided = document.getElementById('tweet-area');
+
+function removeAllChildren(element){
+    while(element.firstChild){//子供要素がある限り削除
+        element.removeChildren(element.firstChild);
+    }
+};
+
+assessmentButton.onclick = function(){
+    const userName = userNameInput.value;
+    if(userName.length === 0){
+        return;
+    }
+
+    removeAllChildren(resultDivided);
+    const header = document.createElement('h3');
+    header.innerText = '診断結果';
+    resultDivided.appendChild(header);
+
+    const paragraph = document.createElement('P');
+    const result = assessment(userName);
+    paragraph.innerText = result;
+    resultDivided.appendChild(paragraph);
+};
+
+const answers = [
+    '{userName}のいいところは声です。',
+    '{userName}のいいところはまなざしです。',
+    '{userName}のいいところは情熱です。',
+    '{userName}のいいところは知識です。',
+    '{userName}のいいところはユニークさです。',
+    '{userName}のいいところは用心深さです。',
+    '{userName}のいいところは見た目です。',
+    '{userName}のいいところは決断力です。',
+    '{userName}のいいところは思いやりです。',
+];
+
+function assessment(userName){
+    
+    let sumOfCharCode = 0;
+    for(let i = 0; i < userName.length; i++){
+        sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
+    }
+
+    const index = sumOfCharCode% answers.length;
+    let result = answers[index];
+
+    result = result.replace(/\{userName\}/g, userName);
+
+    return result;
+}
+
